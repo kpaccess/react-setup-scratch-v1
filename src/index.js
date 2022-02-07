e0de6caad0372996973a6ab2fd14c49fe97dcb0f
krishna-pradhan-telus <86429229+krishna-pradhan-telus@users.noreply.github.com>
2022-02-06 19:44:10 -0500

    moved react and react dom to devDependencies. Enforced linting rules in settings

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const HelloWorld = () => {
+function HelloWorld() {
   return <h1>Hello World 2231</h1>;
-};
+}
 
-ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "build"),
-    publicPath: "/",
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
   },
-  mode: process.env.NODE_ENV || "development",
-  target: "web",
+  mode: process.env.NODE_ENV || 'development',
+  target: 'web',
   // devTool: "cheap-module-source-map",
   devServer: {
     static: {
-      directory: path.join(__dirname, "src"),
+      directory: path.join(__dirname, 'src'),
     },
     compress: true,
     // port: 8000,
@@ -22,17 +22,17 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
+      template: path.join(__dirname, 'src', 'index.html'),
     }),
   ],
 };
